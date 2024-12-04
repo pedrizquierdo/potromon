186e692ac90c6f794b54dc7bf45eb71ef1264d8c
alang <alang@DESKTOP-JU1TSNL>
2024-12-04 01:07:28 -0700

    Se hicieron los pruebas unitarias de las clases de EntrenadoresForm, EntrenadoresListado, HabilidadForm HabilidadListado y Potrodex

---
+++ src/test/java/mx/itson/fokemones/ui/PotroDexTest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package mx.itson.fokemones.ui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author alang
+ */
+public class PotroDexTest {
+    
+    public PotroDexTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class PotroDex.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        PotroDex.main(args);
+    }
+    
+}
