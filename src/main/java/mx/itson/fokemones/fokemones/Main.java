28f99e7e0252dbc7ef963a05df6b98e7d693d5dd
luismorellb <@luismorellb>
2024-12-04 09:23:51 -0700

    Se documentó un método y clase restante.

--- src/main/java/mx/itson/fokemones/fokemones/Main.java
+++ src/main/java/mx/itson/fokemones/fokemones/Main.java
@@ -7,12 +7,17 @@ package mx.itson.fokemones.fokemones;
 import mx.itson.fokemones.persistencia.Conexion;
 
 /**
- *
+ * Main class that starts the application.
+ * 
+ * This class contains the `main` method, which serves as the entry point for program execution.
+ * 
  * @author zBook
  */
 public class Main {
 /**
+ * Main method of the program.
  * The application connects to the database as soon as the program is executed.
+ * @param args
  */
     public static void main(String[] args) {
         Conexion.obtener();
